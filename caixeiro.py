a8763d97a5e48ffd4a5e144965f82f2440551027
Renato-SI <renatorrbf@gmail.com>
2026-09-09 19:08:38 -0300

    Add: Otimização do calculo das rotas de menor_custo & Lista com melhores_rotas

--- caixeiro.py
+++ caixeiro.py
@@ -1,8 +1,10 @@
-entrada = """4 5
-0 0 0 0 D
-0 A 0 0 0
-0 0 0 0 C
-R 0 B 0 0
+entrada = """6 6
+A 0 B 0 C 0
+0 D 0 E 0 F
+0 0 G 0 H 0
+I 0 0 J 0 K
+0 L 0 0 0 0
+0 0 0 0 0 R
 """
 linhas = [linha.strip() for linha in entrada.strip().splitlines() if linha.strip()]
 dimensoes = linhas[0].split()
@@ -10,8 +12,6 @@ num_linhas = int(dimensoes[0])
 num_colunas = int(dimensoes[1])
 pontos = {}
 matriz = linhas[1:]
-entrega_rotas = {}
-menor_custo = float("inf")
 
 for i in range(num_linhas):
     valores = matriz[i].split()
@@ -26,13 +26,39 @@ entregas = list(pontos.keys())
 print(entregas)
 
 
+# MELHORES ROTAS E CUSTO 
+menor_custo = float("inf")
+melhores_rotas = []
+
+def calculo_manhatan(origem, destino):
+    return abs(origem[0] - destino[0]) + abs(origem[1] - destino[1])
+
 def gerar_passeios(cidades):
     resultado = []
 
     def backtrack(atual, restantes):
+
         if not restantes:
-            resultado.append(atual[:])
+            global menor_custo, melhores_rotas
+            custo_rota_atual = 0
+            ponto_atual = origem
+
+            for entrega in atual:
+                destino = pontos[entrega]
+                custo_rota_atual += calculo_manhatan(ponto_atual, destino)
+                ponto_atual = destino
+
+            custo_rota_atual += calculo_manhatan(ponto_atual, origem)
+
+            if custo_rota_atual < menor_custo:
+                menor_custo = custo_rota_atual
+                melhores_rotas = [atual[:]]
+
+            elif custo_rota_atual == menor_custo:
+                melhores_rotas.append(atual[:])
+
             return
+
         for i in range(len(restantes)):
             atual.append(restantes[i])
             backtrack(atual, restantes[:i] + restantes[i+1:])
@@ -41,26 +67,8 @@ def gerar_passeios(cidades):
     backtrack([], cidades)
     return resultado
 
-rotas = gerar_passeios(entregas)
+gerar_passeios(entregas)
 
-def calculo_manhatan(origem, destino):
-    return abs(origem[0] - destino[0]) + abs(origem[1] - destino[1])
-
-for rota in rotas:
-    custo_total = 0
-    ponto_atual = origem
-    for entrega in rota:
-        destino = pontos[entrega]
-        custo_total += calculo_manhatan(ponto_atual, destino)
-        ponto_atual = destino
-    custo_total += calculo_manhatan(ponto_atual, origem)
-    if custo_total < menor_custo:
-        menor_custo = custo_total
-    rota_tupla = tuple(rota)
-    entrega_rotas[rota_tupla] = custo_total
-    print(f"Rota: {rota}, Custo total: {custo_total}")
-
-rotas_minimas = [k for k, v in entrega_rotas.items() if v == menor_custo]
-
-print(f"Rotas com o menor valor: {rotas_minimas}")
-print(f"Menor valor: {menor_custo}")
+print(  
+      f'O menor custo de rota foi: {menor_custo}\n'
+      f'As melhores rotas foram: {melhores_rotas}')
